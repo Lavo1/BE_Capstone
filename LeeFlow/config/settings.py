abf977f2c5dfdef2a69fa8e019b34ef037eb490f
Lerato Lerama <leratolerama3@gmail.com>
2025-08-31 19:47:46 +0200

    Update settings.py and add Procfile and requirements.txt

--- LeeFlow/config/settings.py
+++ LeeFlow/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0ru2%=0w&t2r76t1xukvxq-%(f%31=4b2ly28q=9!#+9exs#0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
